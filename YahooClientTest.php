5cbec98c16f78c99923ccf96d2e7f7d38821de54
Hirofumi Tanigami <tanigami@gmail.com>
2017-03-31 18:29:59 +0900

    memo

--- YahooClientTest.php
+++ YahooClientTest.php
@@ -2,29 +2,40 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Shippinno\YahooShoppingJp\Api\SearchOrders;
 use Shippinno\YahooShoppingJp\Client;
 use Shippinno\YahooShoppingJp\Api\OrderCount;
 
+
+
 define('YAHOO_APP_ID', 'dj0zaiZpPUZFNTFuUUQ3Q2piSSZzPWNvbnN1bWVyc2VjcmV0Jng9MWY-');
 define('YAHOO_SECRET', 'a71d10c84a7826c7f98aae379f4d33607dff4a87');
-
-$accessToken  = 'VPKdeik1obQySJa1g5iOS4bAyUSxLoccRF6gUseT5EPgqyYOrnbZVUxjrnZ45OeszUlp2mPtpDBAv2P1CGzhpzbqvWGjPw.BC0p_FBlwu3kXkrUxEFMoaFtHiU4AOwo4qmG2acQi5tz7HAyuUOQbFG_ZQz.StN86BvFAQEMJR_1Nx0pOE7Eih79JgtSUMdx6LOsrQlF_gGAsVDWk.nNKDitNtki.kDAOilypVD4n4xhXnGNWxrSGR_EzpACoA02zjGGInFu95_RMHvDVET7ZRWc8O_pOq1vs4mA.T_W.6KT_FlFKK9JIWMD9.ckjaEtUHnwqX0tPFEYp8dOURwsB.yss_PmBj20OsrIt9fB9g7MLdlSENSfgEaaE415nLTp.QMEslIL_Y86YHTApIddwlDfBBqbk1S1XlHIVn.8NZCJO2IT_0IHK29bxORbPpNHTU18yAEBAxdvO_bj1xUlWZZiwiCcH.65Acv9GiAFV4lIuGcg4gE0vTyaP3l2Dcm8HhqrizBUGklcDVbhVRg8tXA1i0pPude9ZtRunrVRUtmcmLthMWpwb84JdHgcT3.q7zViyzdwTL3kkPbyYf.BZIpBERCfclNxMd4t28z_dkkA1cNAfdjR.X2KIQrmaiaifC7vqppU-';
+$accessToken  = '63MhY8Y_i6RDbeknp0tplfZXrjbv1SusQ78NU.4Daf1r67sC68xTkJwb91LFrV90JPzLbx_adIrgGpwyllmZpEKNGRRWlC4q55GJBfAiXYOwgZfvIvAmoqcefMHWxQrh9xRV592fSS2vTEq9JZnFjtRVVvqVM8pzuKVHgqiHPicGfK85wBWBHBUAvL9Y0wlHeks5FYVVrYbrVGBDkbfMQKirE71qD32UmHiUN5hA4XS12FB6lH76daOZAL.vPftODv9eJ8gjeEpLY23sn9ccjXmJjbTBQvqvnIGIATdVoBFzfOKUtdy.ZRx0PCzbvWsSW87qRk3o8G5R5EPoaMM2AYmItpM2VndtVwPR_RwQFT_DVP11ghpDhi6.uQvOZW862MYvPB2awbxmyfzcaLL8w6KD9Dl9Ch3GcSW_Kn5Ipv8bk6VGqluJ3VVf6BhfboeyGD9ALqm92qeWjhJ8lzeWBr9Y8hcxFGRLRzRqtMt4M0.W6_IvFl_XRjz5s8siD1tmsy8X.6l6dbNiCH9UMZL4cMuJWm1Ols94ZLAyriQYKXxRxX5b6E0oy8JWNDjpLzbYseVJN2GVEpPi3WRmPowd9Ld.s4NRkuKaBmVWba_1oi_Z3_KxGqFbeklcLAa4nelYy0EPk94-';
 $refreshToken = '';
 $seller_id    = 'snbx-nxpqe5hm3';
 
 
 $client = new Client($accessToken, $refreshToken);
 $client->setApi(new OrderCount);
+$response = $client->execute([
+    'sellerId' => $seller_id,
+]);
 
-$response = $client->execute(['sellerId' => $seller_id], 'GET');
+//$client->setApi(new SearchOrders);
+//$response = $client->execute([
+//    'Search' => [
+//        'Result' => 50,
+//        'Start' => 1,
+//        'Condition' => [
+//            'OrderId' => 'YO',
+//        ],
+//        'Field' => 'OrderId',
+//    ],
+//    'SellerId' => $seller_id,
+//]);
 
-echo "\n";
 var_dump($response);
 
-$client->setDebug(true);
-$response = $client->execute(['sellerId' => $seller_id], 'GET');
+//$client->setDebug(true);
+//$response = $client->execute(['sellerId' => $seller_id], 'GET');
 
-echo "\n";
-echo "\n";
-echo "\n";
-var_dump($response);
