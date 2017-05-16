2d28989c31d7217a7e16d6bf7de4d3c9d3b675a1
yuji.fukui <yuji.fukui@shippinno.co.jp>
2017-05-16 19:56:49 +0900

    UpdateOrderInfo途中まで

---
+++ src/YahooShoppingJp/Response/UpdateOrderInfoResponse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Shippinno\YahooShoppingJp\Response;
+
+class UpdateOrderInfoResponse extends AbstractResponse
+{
+
+    /**
+     * @param array $data
+     */
+    public function setData($data)
+    {
+        // TODO: Implement setData() method.
+    }
+}
